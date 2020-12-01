f27bcbf0f08bde9c6a1988745e6c836dd2507de3
ilahoti <74747239+ilahoti@users.noreply.github.com>
2020-11-30 22:45:24 -0600

    not in

--- Week12-utility.py
+++ Week12-utility.py
@@ -84,3 +84,13 @@ def intersect(list1, list2):
 
     return result
 
+
+def not_in(list1, list2):
+    result = []
+
+    for i in range(0, len(list1)):
+        if list1[i] not in list2:
+            result.append(list1[i])
+
+    return result
+
